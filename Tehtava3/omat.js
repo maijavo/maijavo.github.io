a1a6948bd6fc4fc28e5798547ec98ce0ea40679d
maija.voutila@myy.haaga-helia.fi <maija.voutila@myy.haaga-helia.fi>
2025-02-20 23:20:08 +0200

    korjauksia koodiin

---
+++ Tehtava3/omat.js
@@ -0,0 +1,31 @@
+function kerro(data) {
+    var teksti = "";
+
+    teksti = "<h1>" + data.otsikko + "</h1>";
+    teksti += "<p>" + data.kuvaus + "</p>";
+    teksti += '<img src="https://maijavo.github.io/Tehtava3/koira.png" alt="Kuva" width="300">';
+    teksti += "<h3>Opintojakso</h3>";
+    teksti += "<p>" + data.opintojakso.nimi + " (" + data.opintojakso.tunnus + "), " + data.opintojakso.opintopisteet + " op</p>";
+
+    teksti += "<h3>Sisältö</h3><ul>";
+    data.sisalto.forEach(aihe => {
+        teksti += "<li>" + aihe + "</li>";
+    });
+    teksti += "</ul>";
+
+    teksti += "<h3>Tekniikat</h3><ul>";
+    data.tekniikat.forEach(tekniikka => {
+        teksti += '<li><a href="' + tekniikka.linkki + '" target="_blank">' + tekniikka.aihe + "</a></li>";
+    });
+    teksti += "</ul>";
+
+    document.getElementById("vastaus").innerHTML = teksti;
+}
+
+
+fetch("https://maijavo.github.io/Tehtava3/oma.json")
+    .then(response => response.json()) 
+    .then(responseJson => kerro(responseJson)) 
+    .catch(error => {
+        document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+    });
